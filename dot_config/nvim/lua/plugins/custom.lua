26db11bcdfbb8861202a3ca1818dbdbc68abb824
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-10-11 19:28:18 +0200

    Add nvim config

---
+++ dot_config/nvim/lua/plugins/custom.lua
@@ -0,0 +1,76 @@
+return {
+  -- add colorschemes
+  { "ellisonleao/gruvbox.nvim" },
+  { "navarasu/onedark.nvim" },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      -- colorscheme = "gruvbox",
+      colorscheme = "onedark",
+    },
+  },
+
+  { "f-person/git-blame.nvim" },
+
+  -- add symbols-outline
+  -- {
+  --   "simrat39/symbols-outline.nvim",
+  --   cmd = "SymbolsOutline",
+  --   keys = { { "<leader>cs", "<cmd>SymbolsOutline<cr>", desc = "Symbols Outline" } },
+  --   config = true,
+  -- },
+
+  -- add telescope-fzf-native
+  {
+    "telescope.nvim",
+    dependencies = {
+      "nvim-telescope/telescope-fzf-native.nvim",
+      build = "make",
+      config = function()
+        require("telescope").load_extension("fzf")
+      end,
+    },
+  },
+
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = { eslint = {} },
+      setup = {
+        eslint = function()
+          require("lazyvim.util").on_attach(function(client)
+            if client.name == "eslint" then
+              client.server_capabilities.documentFormattingProvider = true
+            elseif client.name == "tsserver" then
+              client.server_capabilities.documentFormattingProvider = false
+            end
+          end)
+        end,
+      },
+    },
+  },
+
+  {
+    "rcarriga/nvim-notify",
+    opts = {
+      level = "WARN",
+    },
+  },
+
+  {
+    "echasnovski/mini.indentscope",
+    opts = {
+      draw = {
+        -- Delay (in ms) between event and start of drawing scope indicator
+        delay = 50,
+        animation = require("mini.indentscope").gen_animation.none(),
+      },
+    },
+  },
+
+  -- Disable
+  {
+    "folke/todo-comments.nvim",
+    enabled = false,
+  },
+}
