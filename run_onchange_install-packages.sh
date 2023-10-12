9d9ef8d8e8a3810852933d257c78165541a71056
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-10-12 11:13:51 +0200

    Add MacOS options

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -1,6 +1,13 @@
 #!/bin/sh
 
-brew install --cask iterm2
 brew install neovim
 brew install wifi-password
 brew install node@18
+
+brew install --cask iterm2
+brew install --cask 1password
+brew install --cask slack
+brew install --cask vlc
+
+brew tap homebrew/cask-fonts
+brew install --cask font-hack-nerd-font
--- setup.sh
+++ setup.sh
@@ -2,8 +2,14 @@
 
 xcode-select --install
 
-# Setup macos options
+# Macos options
+# Show path bar on finder
 defaults write com.apple.finder ShowPathbar -bool true
+# Show hidden files
+defaults write com.apple.finder AppleShowAllFiles YES
+# show status bar
+defaults write com.apple.finder ShowStatusBar -bool true
+killall Finder
 
 # Install Homebrew
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
