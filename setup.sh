e01f86f3bf97c3ec5e9b9cff5d91914a2a6e79ce
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-10-14 23:36:53 +0200

    Remove macos options

--- setup.sh
+++ setup.sh
@@ -10,19 +10,6 @@ defaults write com.apple.finder AppleShowAllFiles YES
 # show status bar
 defaults write com.apple.finder ShowStatusBar -bool true
 
-# Use the commands below to increase the key repeat rate on macOS beyond the possible settings via the user interface.
-# The changes aren't applied until you restart your computer.
-# https://apple.stackexchange.com/a/83923
-# Settings: System Preferences » Keyboard » Key Repeat/Delay Until Repeat
-defaults write -g InitialKeyRepeat -int 12
-defaults write -g KeyRepeat -int 3
-
-# Turn on mouse Touch-To-Click
-defaults write com.apple.driver.AppleBluetoothMultitouch.trackpad Clicking -bool true
-defaults write com.apple.AppleMultitouchTrackpad Clicking -bool true
-# Apply touch to click changes without restart
-/System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
-
 killall Finder
 
 # Install Homebrew base packages
