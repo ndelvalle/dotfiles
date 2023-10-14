fa33947fde38fb36b9ee1e0bcd9f581e67d8ebe8
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-10-14 11:57:03 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -13,7 +13,8 @@ killall Finder
 
 # Install Homebrew
 # https://github.com/orgs/Homebrew/discussions/4311#discussioncomment-5240151
-sudo echo "Installing Homebrew"
+sudo echo "Installing Homebrew, password will be requested"
+sudo echo " "
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
 # Add Homebrew to PATH
 (echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ndelvalle/.zprofile
