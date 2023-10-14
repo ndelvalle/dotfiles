26d6f7175c6ccd7be84a125a14c57e90bac6a6d8
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-10-14 12:05:07 +0200

    Update homebrew setup (#1)
    * Update setup.sh
    
    * Add Homebrew to PATH
    
    * Update font-hack-nerd-font setup
    
    * Update run_onchange_install-packages.sh
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Update setup.sh
    
    * Update run_onchange_install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -4,10 +4,16 @@ brew install neovim
 brew install wifi-password
 brew install rtx
 
+brew install --cask arc
 brew install --cask iterm2
 brew install --cask 1password
 brew install --cask slack
 brew install --cask vlc
+brew install --cask transmission
+brew install --cask spotify
+brew install --cask telegram
+brew install --cask raycast
+brew install --cask the-unarchiver
 
 brew tap homebrew/cask-fonts
-brew install --cask font-hack-nerd-font
+brew install font-hack-nerd-font
--- setup.sh
+++ setup.sh
@@ -12,7 +12,12 @@ defaults write com.apple.finder ShowStatusBar -bool true
 killall Finder
 
 # Install Homebrew
+# https://github.com/orgs/Homebrew/discussions/4311#discussioncomment-5240151
+sudo echo " "
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+# Add Homebrew to PATH
+(echo; echo 'eval "$(/opt/homebrew/bin/brew shellenv)"') >> /Users/ndelvalle/.zprofile
+eval "$(/opt/homebrew/bin/brew shellenv)"
 
 # Install Homebrew base packages
 brew install git
