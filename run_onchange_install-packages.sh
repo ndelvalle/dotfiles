ea36e2cd8a0bdaf616cb151cb9cded8e8a1b4f56
Nicolas del Valle <nicolas.delvalle@gmail.com>
2023-10-14 11:55:57 +0200

    Update run_onchange_install-packages.sh

--- run_onchange_install-packages.sh
+++ run_onchange_install-packages.sh
@@ -4,10 +4,13 @@ brew install neovim
 brew install wifi-password
 brew install rtx
 
+brew install --cask arc
 brew install --cask iterm2
 brew install --cask 1password
 brew install --cask slack
 brew install --cask vlc
-
-brew tap homebrew/cask-fonts
-brew install font-hack-nerd-font
+brew install --cask transmission
+brew install --cask spotify
+brew install --cask telegram
+brew install --cask raycast
+brew install --cask the-unarchiver
